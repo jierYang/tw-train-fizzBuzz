de105fc0356a81ab0d42315a5953205d05a7e413
Yang Jie <jiyang@thoughtworks.com>
2018-09-05 20:05:18 +0800

    test invalidate totally

--- spec/fizzBuzz-spec.js
+++ spec/fizzBuzz-spec.js
@@ -1,9 +1,29 @@
 var fizzBuzz = require("../src/fizzBuzz");
 
 describe('fizzBuzz test', () => {
-    it('fizzBuzz return null when given null', () => {
-        var result = fizzBuzz(undefined);
+    describe('invalidate test', ()=>{
+        it('fizzBuzz return invalidate when given undifined', () => {
+            var result = fizzBuzz(undefined);
 
-        expect(result).toEqual("invalidate input");
+            expect(result).toEqual("invalidate input");
+        });
+
+        it('fizzBuzz return invalidate when given null', () => {
+            var result = fizzBuzz(null);
+
+            expect(result).toEqual("invalidate input");
+        });
+
+        it('fizzBuzz return invalidate when given -2', () => {
+            var result = fizzBuzz(-2);
+
+            expect(result).toEqual("invalidate input");
+        });
+
+        it('fizzBuzz return invalidate when given 2.5', () => {
+            var result = fizzBuzz(2.5);
+
+            expect(result).toEqual("invalidate input");
+        });
     });
 });
